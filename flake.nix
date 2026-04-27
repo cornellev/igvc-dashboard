1e2065531619b7abf46c41570cf629516ec6f32e
erics118 <52634785+erics118@users.noreply.github.com>
2026-04-27 16:47:13 -0400

    update flake and gitignore

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
@@ -14,12 +15,28 @@
         "x86_64-linux"
       ];
       perSystem =
-        { pkgs, ... }:
+        { system, ... }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.nix-ros-overlay.overlays.default ];
+          };
+          ros = pkgs.rosPackages.humble;
+        in
         {
           devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              (python314.withPackages (ps: [ ps.pip ]))
-              bun
+            packages = [
+              pkgs.bun
+              pkgs.python3
+              # pkgs.python3Packages.uvicorn
+              # pkgs.python3Packages.websockets
+              # pkgs.python3Packages.pyserial
+              # pkgs.python3Packages.httpx
+              # pkgs.python3Packages.rosbags
+              # pkgs.python3Packages.opencv-python-headless
+              # pkgs.python3Packages.numpy
+              ros.rclpy
+              ros.sensor-msgs
             ];
           };
         };
